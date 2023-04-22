fb17cfd4dbeb8218ffa47ca873bb9f546571f025
William May <wjrm500@gmail.com>
2023-04-22 14:55:31 +0800

    Grouping files by file name, size and last modified date

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ for root, dirs, files in os.walk("D:/"):
         mod_time = os.path.getmtime(file_path)
         size = os.path.getsize(file_path)
         print("Found file:", file_path)
-        # Check if file name already exists in the dictionary
-        if file in file_dict:
-            file_dict[file].append((file_path, size, mod_time))
-            print("Matching file found for file name:", file)
+        # Check if file name, size, and modified time already exist in the dictionary
+        if (file, size, mod_time) in file_dict:
+            file_dict[(file, size, mod_time)].append(file_path)
+            print("Matching file found for file name, size, and modified time:", file)
             print("File path:", file_path)
             print("Size:", size, "bytes", "- Last modified:", datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S'))
         else:
-            file_dict[file] = [(file_path, size, mod_time)]
+            file_dict[(file, size, mod_time)] = [file_path]
 
 # Iterate through the dictionary and print file paths, file sizes, and last modified times
 print("Printing matching file paths, file sizes, and last modified times...")
 count = 0
-for file, path_list in file_dict.items():
+for (file, size, mod_time), path_list in file_dict.items():
     if len(path_list) > 1:
-        print("Matching files found for file name:", file)
-        for path, size, mod_time in path_list:
-            print("File path:", path, "- Size:", size, "bytes", "- Last modified:", datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S'))
+        print("Matching files found for file name, size, and modified time:", file, "- Size:", size, "bytes", "- Last modified:", datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S'))
+        for path in path_list:
+            print("File path:", path)
         print()
 
         count += 1
